712019d248103000de468943cff0622917e99800
NaVindu LakShan <89694202+NaVindu0315@users.noreply.github.com>
2024-06-12 12:49:02 +0530

    initial commit

---
+++ lib/Dashboard.dart
@@ -0,0 +1,48 @@
+import 'package:flutter/material.dart';
+
+class Dashboard extends StatelessWidget {
+  const Dashboard({Key? key}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
+
+class Dash extends StatefulWidget {
+  const Dash({Key? key}) : super(key: key);
+
+  @override
+  State<Dash> createState() => _DashState();
+}
+
+class _DashState extends State<Dash> {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      home: Scaffold(
+        appBar: AppBar(
+          // preferredSize: Size.fromHeight(kToolbarHeight + 20),
+          backgroundColor: Colors.blue,
+          leading: IconButton(
+            icon: Icon(
+              Icons.arrow_back_ios_new_outlined,
+              color: Colors.white,
+            ),
+            onPressed: () {
+              Navigator.pop(context);
+            },
+          ),
+
+          title: Text(
+            'Widgets',
+            style: TextStyle(color: Colors.white),
+          ),
+          iconTheme: IconThemeData(color: Colors.white),
+
+          //centerTitle: true,
+        ),
+      ),
+    );
+  }
+}
